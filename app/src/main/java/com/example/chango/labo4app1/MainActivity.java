61cd3e897bf0bcc450f0917d8e35c862312a32d3
alumnoIdat <leomanarael@gmail.com>
2016-09-05 10:13:48 -0500

    terminado

--- app/src/main/java/com/example/chango/labo4app1/MainActivity.java
+++ app/src/main/java/com/example/chango/labo4app1/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.chango.labo4app1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void mostrarAlerta(View view){
+        Toast toast = Toast.makeText(getApplicationContext(),
+                "¡Has pulsado el botón!",
+                Toast.LENGTH_LONG);
+        toast.show();
     }
 }
